0d7b76493ac8b371ece4b8f5f7876239359f4467
naphtron <naphtron@yahoo.com>
2024-01-06 06:13:20 +0300

    added ussd.py

--- get_mpesa_token.py
+++ get_mpesa_token.py
@@ -1,14 +1,21 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+request = ""
 ######
 def getAccessToken(request):
-    consumer_key = ''
-    consumer_secret = ''
+    consumer_key = os.getenv('CONSUMER_KEY')
+    consumer_secret = os.getenv('CONSUMER_SECRET')
     api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
     req = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
     mpesa_access_token = json.loads(req.text)
     validated_mpesa_access_token = mpesa_access_token['access_token']
     print(f'Access token{mpesa_access_token}')
     print(f'Validated access token: {validated_mpesa_access_token}')
+
+getAccessToken(request)
